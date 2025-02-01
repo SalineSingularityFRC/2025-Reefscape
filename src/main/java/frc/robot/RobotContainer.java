5e4f42261137bcaf2c6180e0b5d982d4c2c44544
Christopher Myers <cmyers734@gmail.com>
2025-02-01 09:16:55 -0500

    Test of USB serial

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.LidarOverUsb;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.commands.DriveController;
 import frc.robot.commands.RumbleCommandStart;
@@ -21,10 +22,12 @@ public class RobotContainer {
     private Limelight lime;
     private CommandXboxController driveController;
     private SendableChooser<String> pathAutonChooser;
+    private LidarOverUsb lidar;
 
     protected RobotContainer() {
         lime = new Limelight();
         drive = new SwerveSubsystem();
+        lidar = new LidarOverUsb();
 
         driveController = new CommandXboxController(Constants.Gamepad.Controller.DRIVE);
 
---
+++ src/main/java/frc/robot/subsystems/LidarOverUsb.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LidarOverUsb extends SubsystemBase {
+    private SerialPort serial;
+
+    public LidarOverUsb() {
+        super();
+
+        serial = new SerialPort(115200, Port.kUSB);
+        serial.enableTermination();
+        serial.setTimeout(1);
+        serial.setFlowControl(SerialPort.FlowControl.kNone);
+        serial.setReadBufferSize(32);
+        serial.setWriteBufferSize(8);
+        serial.setWriteBufferMode(SerialPort.WriteBufferMode.kFlushOnAccess);
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+
+        SmartDashboard.putString("Lidar/Debug/Last Received", serial.readString());
+    }
+}
