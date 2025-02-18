18e0f14f1b927ea4007a4ac37672b8c21ac467f4
heatonn <git@noahheaton.com>
2025-02-17 20:58:58 -0500

    Compresses the lidar into 2 arrays (min and avg per column) and send it over 2 different ids

--- src/main/java/frc/robot/subsystems/LidarOverCAN.java
+++ src/main/java/frc/robot/subsystems/LidarOverCAN.java
@@ -1,53 +1,54 @@
 package frc.robot.subsystems;
 
-import java.nio.ByteBuffer;
+import java.util.HashMap;
 
 import edu.wpi.first.hal.CANStreamMessage;
 import edu.wpi.first.hal.can.CANJNI;
 import edu.wpi.first.hal.can.CANMessageNotFoundException;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LidarOverCAN extends SubsystemBase {
 
     private int handle;
-    
-    
-        public LidarOverCAN() {
-            super();
-            handle = CANJNI.openCANStreamSession(0x1E040000, 0xFFFFFFFF, 1000);
-        
-    }
+    // HashMap to store CAN message IDs (as strings with a "0x" prefix) and their occurrence counts
+    private HashMap<String, Integer> idCountMap = new HashMap<>();
 
+    public LidarOverCAN() {
+        super();
+        handle = CANJNI.openCANStreamSession(0x40000555, 0xFFFFFFF0, 1000);
+    }
 
     @Override
     public void periodic() {
-        //ByteBuffer targetedMessageID = ByteBuffer.allocateDirect(4); // Must be direct
-        //targetedMessageID.asIntBuffer().put(0, 0x8041481);
-        //ByteBuffer timeStamp = ByteBuffer.allocateDirect(4); // Allocate memory for time stamp
-        //byte[] message = new byte[] {};
         CANStreamMessage[] messages = new CANStreamMessage[4];
+
         try {
-            // Return call is data, selection is assigned
+            // Read up to 4 messages from the CAN stream
             CANJNI.readCANStreamSession(handle, messages, 4);
         } catch (CANMessageNotFoundException e) {
-            // Nothing
+            // No messages found, do nothing
         } catch (Exception e) {
-            // Other exception, print it out to make sure user sees it
+            // Print any other exception
             System.out.println(e.toString());
         }
-        
+
         if (messages != null && messages.length > 0) {
-            for( CANStreamMessage msg : messages) {
-                if (msg == null) {continue;}
-            //SmartDashboard.putRaw("Lidar Debug Last CAN", message);
-            //System.out.println("Last Can: " + message.toString());
-            System.out.println(String.format("%08X", msg.messageID));
-            /*for (byte b : message) {
-                System.out.print(String.format("%02X ", b));
-            } */
-            System.out.println(""); 
+            for (CANStreamMessage msg : messages) {
+                if (msg == null) {
+                    continue;
+                }
+                // Format the message ID as a hexadecimal string with "0x" prefix
+                String idStr = String.format("0x%08X", msg.messageID);
+                // Update the count for this message ID in the map
+                idCountMap.put(idStr, idCountMap.getOrDefault(idStr, 0) + 1);
+                for (byte b : msg.data){
+                System.out.print(b + " ");
+                }
+                System.out.println();
+            }
+            // Print the entire hashmap after processing messages
+            System.out.println("CAN ID Count Map: " + idCountMap.toString());
         }
-        }
+
     }
 }
