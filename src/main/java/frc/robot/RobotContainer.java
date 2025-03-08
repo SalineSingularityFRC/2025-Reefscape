dfeb41de2de7c675c310731ced18c67f6ef52e37
Fionn Hartley-Beane <hartleyk@salinehornets.org>
2025-03-08 15:36:20 -0500

    Finished Test Path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -62,7 +62,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("RumbleCommantStop", new RumbleCommandStop(driveController));
 
         // For convenience a programmer could change this when going to competition.
-        boolean isCompetition = true;
+        boolean isCompetition = false;
 
         // Build an auto chooser. This will use Commands.none() as the default option.
         // As an example, this will only show autos that start with "comp" while at
