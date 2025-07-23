43f8212d0ee93ba557656d083c3cabca9e7b4374
Bill Hong <billhong@umich.edu>
2025-07-22 23:29:28 -0400

    Changed driving back to having a deadzone

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -301,9 +301,9 @@ public class SwerveSubsystem extends SubsystemBase {
     // don't move or turn at all
     // 0.08 value can be increased if the joystick is increasingly inaccurate at
     // neutral position
-    if (Math.abs(x) < 0.005
-        && Math.abs(y) < 0.005
-        && Math.abs(rotation) < 0.005) {
+    if (Math.abs(x) < 0.06
+        && Math.abs(y) < 0.06
+        && Math.abs(rotation) < 0.06) {
 
       this.stop();
       return;
